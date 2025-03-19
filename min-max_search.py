d91ab53c1ac4ff3d3e211748ac9ace8ae77a1c1b
Irina13-user <Luzanovairina2009@gmail.com>
2025-03-19 19:19:56 +0300

    adding the opportunity to choice the quantity of numbers

--- min-max_search.py
+++ min-max_search.py
@@ -1,17 +1,33 @@
 answer = input("do you know quantity of numbers? (y/n) ")
-if answer.upper() == "Y" or  answer.upper() == "YES":
+if answer.upper() == "Y" or answer.upper() == "YES":
     count = int(input("enter quantity of numbers: "))
-max_num1 = max_num2 = min_num1 = min_num2 = int(input())
-for _ in range(count - 1):
+    max_num1 = max_num2 = min_num1 = min_num2 = int(input())
+    for _ in range(count - 1):
+        number = int(input())
+        if number < min_num1:
+            min_num2 = min_num1
+            min_num1 = number
+        elif number < min_num2:
+            min_num2 = number
+        elif number > max_num1:
+            max_num2 = max_num1
+            max_num1 = number
+        elif number > max_num2:
+            max_num2 = number
+    print(max_num1, max_num2, min_num1, min_num2)
+else:
+    max_num1 = max_num2 = min_num1 = min_num2 = int(input())
     number = int(input())
-    if number < min_num1:
-        min_num2 = min_num1
-        min_num1 = number
-    elif number < min_num2:
-        min_num2 = number
-    elif number > max_num1:
-        max_num2 = max_num1
-        max_num1 = number
-    elif number > max_num2:
-        max_num2 = number
-print(max_num1, max_num2, min_num1, min_num2)
+    while number:
+        if number < min_num1:
+            min_num2 = min_num1
+            min_num1 = number
+        elif number < min_num2:
+            min_num2 = number
+        elif number > max_num1:
+            max_num2 = max_num1
+            max_num1 = number
+        elif number > max_num2:
+            max_num2 = number
+        number = int(input())
+    print(max_num1, max_num2, min_num1, min_num2)
