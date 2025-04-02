d5d664585b97195ad0987cbe967e35e34188893e
Irina13-user <Luzanovairina2009@gmail.com>
2025-04-02 18:31:22 +0300

    fixing the mistakes in functions

--- Functions.py
+++ Functions.py
@@ -1,33 +1,25 @@
-def quantity_of_numbers(number):
+def quantity_of_digits(number):
+    count = 0
     while number:
-        count = 0
-        number // 10
+        number //= 10
         count += 1
     return count
-def sum_of_numbers(number):
+def sum_of_digits(number):
+    sum = 0
     while number:
-        sum = 0
         sum += number % 10
+        number //= 10
     return sum
 def quantity_of_divisors(number):
-    quantity = 1
-    if number % 2 == 0:
-        quantity += 1
-    elif number % 3 == 0:
-        quantity += 1
-    elif number % 5 == 0:
-        quantity += 1
-    elif number % 7 == 0:
-        quantity += 1
-    elif number % 11 == 0:
-        quantity += 1
-    elif number % 13 == 0:
+    quantity = 2
+    for divisor in range(2, int(number ** .5)):
+        if number % divisor == 0:
+            quantity += 2
+    if number % (int(number ** .5)) == 0:
         quantity += 1
     return quantity
 
 number = int(input())
-print(quantity_of_numbers(number))
-print(sum_of_numbers(number))
+print(quantity_of_digits(number))
+print(sum_of_digits(number))
 print(quantity_of_divisors(number))
-
-
