7c4d903e6cc35e726852b15211cd784b0f529324
Irina13-user <Luzanovairina2009@gmail.com>
2025-07-30 19:59:50 +0300

    add files.py

---
+++ multistring file.py
@@ -0,0 +1,8 @@
+with open('multistring.txt', encoding='UTF-8') as file:
+    lines = file.readlines()
+for i in range(len(lines)):
+    if "\n" in lines[i]:
+        lines[i] = lines[i][:-1]
+max_len = max(lines, key=len)
+print(lines)
+print(max_len)
