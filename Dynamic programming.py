7c4545259b6ea7127ff139022ae255fd319e4a13
Irina13-user <Luzanovairina2009@gmail.com>
2025-07-30 18:44:54 +0300

    correct mistakes, add dictionary

--- Dynamic programming.py
+++ Dynamic programming.py
@@ -1,14 +1,14 @@
 numbers = [0] * 25
 numbers[2] = 1
-for i in range (3, 25):
-    if i == 12:
-        continue
+for i in range (3, 11):
     numbers[i] = numbers[i - 1]
     if i % 2 == 0:
         numbers[i] += numbers[i // 2]
-    elif i == 11:
-        continue
+numbers[11] = numbers[10]
+numbers[12] = numbers[6]
+for i in range (13, 25):
     numbers[i] = numbers[i - 1]
-    if i % 2 == 0 and (i < 11 or i > 20):
+    if i % 2 == 0 and i < 20:
         numbers[i] += numbers[i // 2]
+numbers = {n: numbers[n] for n in range(25)}
 print(numbers)
